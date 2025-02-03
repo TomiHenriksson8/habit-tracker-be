c5470df7f4ced1f3c675aad10e5ae01c73e060bf
TomiHenriksson8 <tomihen01@gmail.com>
2025-02-03 16:14:47 +0100

    Add login, register routes, JWT middleware

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,34 @@ mod models;
 mod routes;
 mod utils;
 
-fn main() {
-    println!("Hello world!");
+use axum::{routing::get, Json, Router};
+use serde::Serialize;
+use std::net::SocketAddr;
+use tower_http::trace::TraceLayer;
+
+#[derive(Serialize)]
+struct HealthCheckResponse {
+    status: String,
+}
+
+async fn health_check() -> Json<HealthCheckResponse> {
+    Json(HealthCheckResponse {
+        status: "Server is running".to_string(),
+    })
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(health_check))
+        .nest("/api/auth", routes::auth::auth_routes())
+        .layer(TraceLayer::new_for_http());
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+
+    println!("Server running at http://{}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .expect("Server failed to start")
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,33 @@
+use argon2::password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString};
+use argon2::Argon2;
 use chrono::{Duration, Utc};
 use dotenvy::dotenv;
 use jsonwebtoken::{encode, EncodingKey, Header};
+use rand::rngs::OsRng;
 use serde::{Deserialize, Serialize};
 use std::env;
 
 pub fn get_secret_key() -> String {
     dotenv().ok();
     env::var("SECRET_KEY").expect("SECRET_KEY must be set")
+}
+pub fn hash_password(password: &str) -> String {
+    let salt = SaltString::generate(&mut OsRng); // Generate a random salt
+    let argon2 = Argon2::default();
+
+    argon2
+        .hash_password(password.as_bytes(), &salt)
+        .expect("Password hashing failed")
+        .to_string()
+}
+
+pub fn verify_password(password: &str, hashed_password: &str) -> bool {
+    let parsed_hash = PasswordHash::new(hashed_password).expect("Invalid hash format");
+    let argon2 = Argon2::default();
+
+    argon2
+        .verify_password(password.as_bytes(), &parsed_hash)
+        .is_ok()
 }
 #[derive(Serialize, Deserialize)]
 struct Claims {
